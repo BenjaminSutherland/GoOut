202569329d809196c189b1253151027635ecc70d
josuemunro <josuemunro@gmail.com>
2017-03-26 22:18:45 +1300

    Added Facebook loginscripts and edited loginpage

---
+++ loginpage.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>come out</title>
+	<meta name="author" content="Antonio Pratas">
+	<link href="index.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="js/javascript.js"> </script>
+    <script type="text/javascript" src="./fbapp/fb.js"></script>
+</head>
+
+<body class="loginbackground">
+<div class="fb-login-button"></div>
+
+</body>
+</html>
